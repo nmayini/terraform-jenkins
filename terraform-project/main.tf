d2515862d1edd7def910ade7660f7d760e65cd2b
Nikhil <nikhilmayini9@gmail.com>
2026-05-15 11:10:19 +0530

    added main.tf to the terraform-project

---
+++ terraform-project/main.tf
@@ -0,0 +1,12 @@
+terraform {
+required_version = ">= 1.0.0"
+required_providers {
+local = {
+source = "hashicorp/local"
+version = "~> 2.5"
+}
+}
+}
+resource "local_file" "demo" {
+filename = "demo.txt"
+content = "Hello Nikhil Mayini"
